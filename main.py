4d8771a33176321751c92c2329eb0ccb703a5b2f
Ghost <qpages08@gmail.com>
2025-12-02 20:20:57 +0100

    corriger grosses erreurs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import pyxel
 import webbrowser
+from random import randint
 
 
         
@@ -10,8 +11,9 @@ class Game: #classe qui cree le jeu et qui possede la boucle de jeu
         self.height = height
         self.nom = nom_jeu
         self.hitbox = True
+        self.pos_cible = [0,0]
         
-        self.menu_actuel = "None" #None, GameOver, Start, Pause
+        self.menu_actuel = "Start" #Playing, GameOver, Start, Pause
         
 
         pyxel.init(self.width, self.height)
@@ -26,26 +28,43 @@ class Game: #classe qui cree le jeu et qui possede la boucle de jeu
     def update(self):
         
         
-        if self.menu_actuel == "None":
+        if self.menu_actuel == "Playing":
             
             if self.player.is_alive(): # boucle du jeu qui verifie si le joueur est mort
                 #ici si le player est vivant
                 #mettre la suite du jeu ici
                 self.player.move()
-                
-                
+                if pyxel.frame_count %90 ==0:
+                    self.liste_mob.append(Mob(10,10,10,10))
+                    # faire apparaitre les mobs dans une liste
+
+                for mob in self.liste_mob:
+                    if pyxel.frame_count % 15 ==0:
+                        self.pos_cible = [self.player.x,self.player.y] #envoie cible des mobs pour ajouter un deplacement moins linéaire
+
+
+                    mob.move(self.pos_cible)
+                    if not mob.is_alive():
+                        self.liste_mob.remove(mob)
+
+                    
+
+
+
             else:#si le joueur est mort
                 print(self.player.nom, "est mort")
+
+        if self.menu_actuel == "Start":
+        # if  self.menu_principal.is_showed:
+            if pyxel.btn(pyxel.KEY_RETURN) or pyxel.btn(pyxel.KEY_KP_ENTER):
+                self.menu_actuel  = "Playing"
                 
-            #test de la mort des Mobs
-            for mobs in self.liste_mob:
-                if mobs.is_alive() == True:
-                    self.liste_mob.remove(mobs)
-                    
-            # update des balles de l'arc:
-                #...
                 
-            self.player.arme_active.update_attaque()
+            
+                
+
+                
+        self.player.arme_active.update_attaque()
          
         if self.menu_actuel == "GameOver":
               webbrowser.open_new("https://www.youtube.com/watch?v=xvFZjo5PgG0")
@@ -60,13 +79,18 @@ class Game: #classe qui cree le jeu et qui possede la boucle de jeu
     def draw(self):
         pyxel.cls(0)
         
-        self.menu()    
+        self.menu()#debug    
             
-        if self.menu_actuel =="None":
+        if self.menu_actuel =="Playing":
             if self.hitbox:
                 self.player.draw_hitbox()
     
             self.player.draw()
+
+            for mob in self.liste_mob:
+                    if self.hitbox:
+                        mob.draw_hitbox()
+                    mob.draw()
             
     def menu(self):
         if self.menu_actuel == "GameOver":
@@ -78,8 +102,8 @@ class Game: #classe qui cree le jeu et qui possede la boucle de jeu
         elif self.menu_actuel == "Start":
             print('Menu de départ')
             
-        elif self.menu_actuel =="None":
-            print('rien à faire')
+        elif self.menu_actuel =="Playing":
+            print('partie en cours')
     
     def changer_menu(self, menu_remplacement):
         self.menu_actuel = str(menu_remplacement)
@@ -126,8 +150,6 @@ class Armes:
             
                 
             
-    def update(self):
-        pass
             
     
     # def caracteristique(self):
@@ -276,7 +298,7 @@ class Player: #classe qui cree le joueur
             pyxel.rect(1, 1, length*(self.vie/self.vie_max), height, col)
         else:
             print('The END')#MENU de FIN
-            Game.changer_menu("GameOver")
+            
         
          
             
@@ -292,18 +314,50 @@ class Player: #classe qui cree le joueur
 
 
 class Mob:
-    def __init__(self, life, damage, attack_speed, speed):
+    def __init__(self, life:int, damage:int, attack_speed:int, vitesse:int):
         """initialisation de la creation de mob"""
         self.life = life
         self.damage = damage
         self.attack_speed = attack_speed
-        self.speed = speed
+        self.vitesse = vitesse
+        self.vitesse = 1
+        self.x = randint(0,pyxel.width-7)
+        self.y = 0
+        self.cooldown_state = 3
+        self.cooldown_max = randint(3,7)
     
     def update(self):
         pass
     
+    def draw_hitbox(self):
+            pyxel.rect(self.x-1,self.y-1,7,7,9) #7= taille mob + 2 pour que l'on voie un peu le rectangle
+
+    def move(self, tableau_cible:list):
+        """Prend en parametre tableau ontenatn les coordonnes cibles vers lesquels ils doivent se deplacer 
+        tableau sous forme [x,y]
+        la variable cooldown existe pour que les mobs se deplacent de faconc plus saccadees"""
+
+
+        if self.peut_bouger():
+            #verifie si le mob peut jouer -> verifie son cooldown est ok
+            player_x = tableau_cible[0]
+            player_y = tableau_cible[1]
+            mob_x = self.x
+            mob_y = self.y
+            if player_y-5 >= mob_y:
+                self.y += self.vitesse
+
+            elif player_y+5 <= mob_y:
+                self.y -= self.vitesse
+
+            if player_x+5 <= mob_x:
+                self.x -= self.vitesse
+
+            elif player_x-5 >= mob_x:
+                self.x += self.vitesse
+
     def draw(self):
-        pass
+        pyxel.rect(self.x,self.y,5,5,11)
     
     def degat(self):
         """change la vie du mob"""
@@ -314,6 +368,16 @@ class Mob:
         if self.life > 0:
             return True
         elif self.life <= 0:
+            return False
+        
+    def peut_bouger(self):
+        """renvoie True si le mob peut bouger
+        sinon renvoie False"""
+        if self.cooldown_state == 0:
+            self.cooldown_state = self.cooldown_max
+            return True
+        else:
+            self.cooldown_state -=1
             return False
         
 
