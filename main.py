4c7a71ad90ff806ef05ba2bb5dcc1ff48275cb47
vabregaetan-svg <vabregaetan@gmail.com>
2026-01-28 22:52:40 +0100

    importation des graphismes du player avec le sniper (version 1.0)

--- main.py
+++ main.py
@@ -722,7 +722,14 @@ class Player: #classe qui cree le joueur
         if self.is_alive():
             pyxel.rect(self.x,self.y,5,5,6)
             self.draw_health()
-        pyxel.blt(self.x, self.y, 0, 0, 24, 8, 8)
+        if pyxel.btn(pyxel.KEY_DOWN):
+            pyxel.blt(self.x, self.y, 0, 8, 48, 8, 8)
+        if pyxel.btn(pyxel.KEY_UP):
+            pyxel.blt(self.x, self.y, 0, 0, 48, 8, 8)
+        if pyxel.btn(pyxel.KEY_LEFT):
+            pyxel.blt(self.x, self.y, 0, 8, 56, 8, 8)
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            pyxel.blt(self.x, self.y, 0, 0, 56, 8, 8)
 
 
     def draw_hitbox(self):
